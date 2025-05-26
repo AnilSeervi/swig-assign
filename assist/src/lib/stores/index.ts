6b5021a24326f67644e8718b0527e30cc456cbbb
Anil Seervi <61609033+AnilSeervi@users.noreply.github.com>
2025-05-26 12:20:51 +0530

    Delete assist directory

--- assist/src/lib/stores/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './useBookingStore';
-export * from './useConversationStore';
--- assist/src/lib/utils/api.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Simulates an API call delay for development purposes
- * @param delay - Delay in milliseconds
- */
-export const simulateAPIDelay = (delay: number = 500) => 
-  new Promise(resolve => setTimeout(resolve, delay));
